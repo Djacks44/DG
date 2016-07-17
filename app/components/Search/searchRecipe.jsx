d482b438c48a35319d63402ffed38012e475cb71
williamavasquez <william.a.vasquez@gmail.com>
2016-07-17 14:03:17 -0400

    working React Form

--- app/components/Search/searchRecipe.jsx
+++ app/components/Search/searchRecipe.jsx
@@ -1,37 +1,35 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 
-class SearchRecipe extends Component {
-  constructor() {
+
+class SearchRecipe extends React.Component {
+    constructor() {
     super();
     this.state = {
       search:[],
       sTerm:""
-    }
-
-  handleClick: function(){
-    let sTerm = this.ref.searchterm
-    console.log(sTerm);
-    axios.get("http://api2.bigoven.com/recipes?pg=1&rpp=25&title_kw=chicken&api_key=3r23I5wV7rQo5zv899t13KaKjFpJW40K")
-    .then(function(response){console.log(response)});
-  )
+    };
   }
 
-  render: function(){
+  handleClick(){
+   var x = this.refs.searchTerm;
+   console.log(x);
+   axios.get("http://api2.bigoven.com/recipes?pg=1&rpp=25&title_kw=chicken&api_key=3r23I5wV7rQo5zv899t13KaKjFpJW40K")
+   .then(function(response){console.log(response)});
+  }
+
+  render(){
+    return(
     <div>
-
-    <form className="new-resolution" onSubmit={this.search.bind(this)}>
-      <input type="text" ref="searchTerm" placeholder="What do you want to Eat?" />
-      <button onClick={this.handleClick}>Submit</button>
-    </form>
-
+      <form className="new-resolution" onSubmit = {this.handleClick.bind(this)}>
+        <input type="text" ref="searchTerm" placeholder="What do you want to Eat?" />
+        <button onClick={this.handleClick}>Submit</button>
+     </form>
     </div>
+  );
   }
-
-
-  }
-
-
 }
+
+React.render(<SearchRecipe />, document.getElementById("container"))
 
 export default SearchRecipe
