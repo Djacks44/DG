25fde6979dafa06c4b087501ac629c7f739dd020
williamavasquez <william.a.vasquez@gmail.com>
2016-07-17 11:53:40 -0400

    logic file update

--- server/logic/logic.js
+++ server/logic/logic.js
@@ -5,22 +5,17 @@ var request = require('request');
 
 
 router.post('/',function(req,res){
-    y = req.body.searchterm
-    y = y.trim();
+    TitleKeyword = req.body.searchterm.trim()
+    var url = "http://api2.bigoven.com/recipes?pg=1&rpp=25&title_kw="
+              + TitleKeyword
+              + "&api_key=3r23I5wV7rQo5zv899t13KaKjFpJW40K";
 
-        var TitleKeyword = y;
-
-        var url = "http://api2.bigoven.com/recipes?pg=1&rpp=25&title_kw="
-                  + TitleKeyword
-                  + "&api_key=3r23I5wV7rQo5zv899t13KaKjFpJW40K";
-
-              request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                      body = JSON.parse(body)
-                      res.json(body.Results);
-                        console.log(y);
-                      }
-                    })
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          body = JSON.parse(body)
+          res.json(body.Results);
+      }
+    })
 });
 
 
